872c4cacab1592a7ef254e9ab9156dd6d5ada009
Yoni Baciu <yoni.baciu@persado.com>
2024-03-17 09:28:48 -0400

    another ui change to test workflow

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Dockerized NextJS app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker :|
+          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker :)
         </h1>
 
         <p className={styles.description}>
