c6eca546602c72186ada997fb8a9555c024cbe3b
Yoni Baciu <yoni.baciu@persado.com>
2024-03-18 22:06:50 -0400

    remove unneeded attrs from the aws task-definition.json file

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker ❤️
+          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker 👍
         </h1>
 
         <p className={styles.description}>
