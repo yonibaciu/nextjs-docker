d093a07f52bd9018f86e25e1bf68990aa25fe89f
Yoni Baciu <yoni.baciu@persado.com>
2024-03-19 11:45:35 -0400

    test github workflow

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker 👍
+          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker 👍👍
         </h1>
 
         <p className={styles.description}>
