9d002d70f848bd07d09d5c53122d4a33e07a7406
Yoni Baciu <yoni.baciu@persado.com>
2024-03-20 09:44:52 -0400

    another dummy chanage

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker 👍👍👍
+          Welcome to Yoni's <a href="https://nextjs.org">Next.js</a> on Docker 👍👍👍👍
         </h1>
 
         <p className={styles.description}>
